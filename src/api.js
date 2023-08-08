3c7974313f7e3ea1500368f7a46cbf7d99453205
BoSeon Park <bedurgi999@gmail.com>
2023-08-08 18:47:31 +0900

    first commit

---
+++ src/api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+const serverUrl = "http://localhost:8080/";
+
+async function get(endpoint, params = "") {
+  return axios.get(serverUrl + endpoint + "/" + params);
+}
+
+async function post(endpoint, data) {
+  const bodyData = JSON.stringify(data);
+
+  return axios.post(serverUrl + endpoint, bodyData, {
+    "Content-Type": "application/json",
+  });
+}
+
+async function put(endpoint, data) {
+  // JSON.stringify 함수: Javascript 객체를 JSON 형태로 변환함.
+  // 예시: {name: "Kim"} => {"name": "Kim"}
+  const bodyData = JSON.stringify(data);
+
+  return axios.put(serverUrl + endpoint, bodyData, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
+
+async function del(endpoint, params = "") {
+  return axios.delete(serverUrl + endpoint + "/" + params);
+}
+
+export { get, post, put, del as delete };
