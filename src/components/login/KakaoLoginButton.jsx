b9372cbcea8d73a571ed29592868e0cbb3da72fa
BoSeon Park <bedurgi999@gmail.com>
2023-08-18 17:31:49 +0900

    package rename

--- src/components/login/KakaoLoginButton.jsx
+++ src/components/login/KakaoLoginButton.jsx
@@ -3,19 +3,17 @@ import kakaoLoginImg from '../../assets/login/kakao_login_large_wide.png';
 
 function KakaoLoginButton() {
   const Rest_api_key = process.env.REACT_APP_KAKAO_REST_API_KEY; //REST API KEY
-  const redirect_uri = 'http://localhost:3000/auth' //Redirect URI
+  const redirect_uri = 'http://localhost:3000/auth'; //Redirect URI
   // oauth 요청 URL
-  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`
-  const handleLogin = ()=>{
-    window.location.href = kakaoURL
-  }
+  const kakaoURL = `https://kauth.kakao.com/oauth/authorize?client_id=${Rest_api_key}&redirect_uri=${redirect_uri}&response_type=code`;
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
   return (
     <div>
-      <img src={kakaoLoginImg} alt="kakaoLoginButton" onClick={handleLogin}/>
+      <img src={kakaoLoginImg} alt="kakaoLoginButton" onClick={handleLogin} />
     </div>
-  )
+  );
 }
 
-
-export default KakaoLoginButton
-
+export default KakaoLoginButton;
