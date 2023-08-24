660e9f18c2e6c9c10cebb7a0eb3e79ca34bd2ad4
seolhi <seolhi1212@gmail.com>
2023-08-24 17:09:40 +0900

    커밋이 왜 두번...

--- src/pages/CustomPage.jsx
+++ src/pages/CustomPage.jsx
@@ -7,18 +7,17 @@ import { PiBoneLight } from 'react-icons/pi';
 import '../styles/CustomPage.css'; // 추가한 스타일 파일을 import
 
 function CustomPage() {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const fileInputRef = useRef(null);
 
-    const [selectedImage, setSelectedImage] = useState(null);
-    const fileInputRef = useRef(null);
+  const handleImageUpload = (event) => {
+    const file = event.target.files[0];
+    setSelectedImage(URL.createObjectURL(file));
+  };
 
-    const handleImageUpload = (event) => {
-        const file = event.target.files[0];
-        setSelectedImage(URL.createObjectURL(file));
-    };
-
-    const handleUploadButtonClick = () => {
-        fileInputRef.current.click();
-    };
+  const handleUploadButtonClick = () => {
+    fileInputRef.current.click();
+  };
 
   return (
     <div className="custom-page">
@@ -41,7 +40,7 @@ function CustomPage() {
       {/* 흰디 안내말 */}
       <div className="heendy-say">
         <div className="bubble-box">
-            <p className="bubble-text">퍼스널 쇼퍼 흰디에요!</p>
+          <p className="bubble-text">퍼스널 쇼퍼 흰디에요!</p>
         </div>
       </div>
 
@@ -50,8 +49,10 @@ function CustomPage() {
         {/* 나의 반려동물 선택 부분 */}
         <div className="upload-section">
           <hr className="divider" />
-          
-          <p className="box-title"><PiBoneLight /> 나의 반려동물</p>
+
+          <p className="box-title">
+            <PiBoneLight /> 나의 반려동물
+          </p>
           <hr className="divider" />
           <img src={logo} alt="Pet" className="pet-image" />
           <p className="pet-name">반려동물 이름</p>
@@ -61,7 +62,9 @@ function CustomPage() {
         {/* 기존의 사료 사진 업로드 부분 */}
         <div className="upload-section">
           <hr className="divider" />
-          <p className="box-title"><PiBoneLight /> 나의 반려동물이 잘 먹는 사료</p>
+          <p className="box-title">
+            <PiBoneLight /> 나의 반려동물이 잘 먹는 사료
+          </p>
           <hr className="divider" />
           <div className="image-preview">
             {selectedImage && <img src={selectedImage} alt="Uploaded" />}
@@ -74,15 +77,20 @@ function CustomPage() {
             ref={fileInputRef}
             style={{ display: 'none' }} // 숨김 처리
           />
-          <button className="feed-upload-button" onClick={handleUploadButtonClick}>
+          <button
+            className="feed-upload-button"
+            onClick={handleUploadButtonClick}
+          >
             사료 성분표 업로드
           </button>
         </div>
       </div>
       <p>성분 분석 완료!</p>
-      <p>현대 더펫의 OCR 시스템과 AI 기반으로 분석한 결과, 가장 성분이 유사한 사료를 찾은 결과입니다.</p>
+      <p>
+        현대 더펫의 OCR 시스템과 AI 기반으로 분석한 결과, 가장 성분이 유사한
+        사료를 찾은 결과입니다.
+      </p>
       <hr className="divider" />
-
     </div>
   );
 }
