01f84f1e521ac85e287aa96566a333770f79e8bf
송민진 <m_6595@naver.com>
2023-08-31 15:58:19 +0900

    feat(sub): 구독 UI 1차 구현 (모달 구현 필요)
    #13

--- src/components/subscription/index.style.jsx
+++ src/components/subscription/index.style.jsx
@@ -11,7 +11,7 @@ export const SubMainAdv = styled.div`
 `;
 
 export const SubMainAdvImg = styled.img`
-  max-width: 100%;
+  width: 100%;
   height: auto;
 `;
 
@@ -57,4 +57,69 @@ export const TpbCardImg = styled.img`
   height: 13vw;
   border-radius: 2vw;
   box-shadow: 0 0.3vw 0.3vw rgba(0, 0, 0, 0.3);
+`;
+
+export const TpbMainSect = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* background-color: #C9A581; */
+  background-color: #dac6b2;
+`;
+
+export const TpbMainSectBox = styled.div`
+  width: 40%;
+  height: 30vw;
+  margin: 3vw 5vw;
+  border-radius: 2vw;
+  background-color: white;
+`;
+
+export const TpbMainBox = styled.div`
+  margin: 2.5vw 3vw;
+`;
+
+export const TpbMainSectDescr = styled.img`
+  width: 57%;
+  height: auto;
+  display: block;
+  margin: 3vw auto;
+`;
+
+export const TpbMainBoxTitle = styled.div`
+  font-size: 1.4vw;
+  font-weight: bolder;
+  margin-bottom: 0.7vw;
+`;
+
+export const TpbMiniMark = styled.img`
+  width: 5vw;
+  margin-bottom: 1.5vw;
+`;
+
+export const TpbMainBoxDescr = styled.div`
+  font-size: 0.4vw;
+  color: darkgray;
+  margin-bottom: 3vw;
+`;
+
+export const TpbMainContentBox = styled.div`
+  margin-top: 1vw;
+  width: 100%;
+  height: 3vw;
+  color: gray;
+  background-color: #EBEBEB;
+  border-radius: 0.8vw;
+  /* font-weight: bold; */
+  font-size: 0.8vw;
+  display: flex;           // Flexbox 활성화
+  align-items: center;     // 수직 중앙 정렬
+`;
+
+export const TpbMiniIcon = styled.img`
+  width: 1.5vw;
+  height: 1.5vw;
+  padding-left: 1vw;
+  margin-right: 0.7vw;
+  object-fit: contain;
 `;
