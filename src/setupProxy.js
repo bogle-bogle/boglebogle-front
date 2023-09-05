282190ba176fc3a8d9d4ae94af8d58b688c4039f
송민진 <m_6595@naver.com>
2023-09-05 16:54:41 +0900

    fix: url 중복 제거

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,6 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function (app) {
   app.use(
     createProxyMiddleware('/api', {
-      target: 'http://localhost:8080',
       target: 'http://52.78.209.66:8080',
       changeOrigin: true,
     }),
