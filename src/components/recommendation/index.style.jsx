fd0b4edc7e5369691a1c5d6c89c54688e4dd41f7
송민진 <m_6595@naver.com>
2023-09-05 09:42:55 +0900

    style: 코드 정렬

--- src/components/recommendation/index.style.jsx
+++ src/components/recommendation/index.style.jsx
@@ -1,15 +1,15 @@
 import styled from 'styled-components';
 
 export const RcContainer = styled.div`
-    margin: 0 auto;
+  margin: 0 auto;
 `;
 
 export const RcTitle = styled.div`
-    font-size: 25px;
+  font-size: 25px;
 
-    @media (max-width: 768px) {
-        font-size: 20px;
-    }
+  @media (max-width: 768px) {
+    font-size: 20px;
+  }
 
-    font-weight: bold;
+  font-weight: bold;
 `;
