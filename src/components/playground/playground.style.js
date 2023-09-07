65a21a3e03a2d5b045d6fda030e079f5e395953d
송민진 <m_6595@naver.com>
2023-09-07 19:28:33 +0900

    feat(playground): 놀이터 연결
    #64

---
+++ src/components/playground/playground.style.js
@@ -0,0 +1,29 @@
+import styled from 'styled-components';
+
+export const PlaygroundGrid = styled.div`
+    width: 100%;
+    height: 100%;
+    margin-top: 5%;
+    text-align: center;
+`;
+
+export const PlaygroundTitle = styled.div`
+    font-size: 2vw;
+    font-weight: bolder;
+    margin: 0 auto;
+    padding: 0 auto;
+    `
+
+export const PlaygroundBtnContainer = styled.div`
+    margin: 5% auto 10%;
+    display: flex;
+    height: 100%;
+    width: 100%;
+`
+
+export const PlaygroundMainBtn = styled.img`
+    width: 40%;
+    margin: 0 auto;
+    object-fit: contain;
+    cursor: pointer;
+`;
