925ebc83a7976f951aa2d65b7df64c408a846d28
송민진 <m_6595@naver.com>
2023-09-07 22:33:28 +0900

    style: 코드 정렬

--- src/components/playground/PlayGroundContainer.jsx
+++ src/components/playground/PlayGroundContainer.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import { PlaygroundBtnContainer, PlaygroundGrid, PlaygroundMainBtn, PlaygroundTitle } from './playground.style';
-import MbtiBtnImg from '../../assets/playground/mbti_btn.png'
-import CardBtnImg from '../../assets/playground/card_btn.png'
+import {
+  PlaygroundBtnContainer,
+  PlaygroundGrid,
+  PlaygroundMainBtn,
+  PlaygroundTitle,
+} from './playground.style';
+import MbtiBtnImg from '../../assets/playground/mbti_btn.png';
+import CardBtnImg from '../../assets/playground/card_btn.png';
 import { useNavigate } from 'react-router';
 
 function PlayGroundContainer() {
@@ -15,7 +20,7 @@ function PlayGroundContainer() {
         <PlaygroundMainBtn src={CardBtnImg} onClick={() => navigate('/card')} />
       </PlaygroundBtnContainer>
     </PlaygroundGrid>
-  )
+  );
 }
 
 export default PlayGroundContainer;
