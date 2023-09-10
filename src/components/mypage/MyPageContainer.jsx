1dece706a370edc9ab8a2b5ead97dcecf81ebfed
송민진 <m_6595@naver.com>
2023-09-10 13:37:32 +0900

    feat(mypage): 마이페이지 UI 구현

--- src/components/mypage/MyPageContainer.jsx
+++ src/components/mypage/MyPageContainer.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import { MypageAdv, MypageAdvBtn, MypageAdvImg, MypageBoldBorder, MypageBorder, MypageContent, MypageGrid, MypageMiniTitle, MypageSidebar, MypageSubtitle, MypageTitle } from './mypage.style';
+import ClubAdvImg from '../../assets/club/join_club_adv_narrow.png'
+import { useNavigate } from 'react-router-dom';
 
 function MyPageContainer() {
-  return <div></div>;
+  const navigate = useNavigate();
+
+  return (
+  <MypageGrid>
+
+    <MypageSidebar>
+      <MypageTitle>마이 페이지</MypageTitle>
+      <MypageBoldBorder />
+
+      <MypageSubtitle>나의 반려동물 관리</MypageSubtitle>
+      <MypageMiniTitle>나의 반려동물 목록</MypageMiniTitle>
+      <MypageMiniTitle>나의 반려동물 맞춤 추천</MypageMiniTitle>
+      <MypageBorder />
+
+      <MypageSubtitle>나의 쇼핑내역</MypageSubtitle>
+      <MypageMiniTitle>주문/배송 조회</MypageMiniTitle>
+      <MypageMiniTitle>구독 관리</MypageMiniTitle>
+      <MypageBorder />
+
+      <MypageSubtitle>나의 흰디카 예약</MypageSubtitle>
+      <MypageMiniTitle>자주 찾는 지점</MypageMiniTitle>
+      <MypageMiniTitle>예약/취소 내역</MypageMiniTitle>
+      <MypageBorder />
+
+      <MypageSubtitle>회원 정보</MypageSubtitle>
+      <MypageMiniTitle>회원정보 수정</MypageMiniTitle>
+      <MypageMiniTitle>배송지 관리</MypageMiniTitle>
+      <MypageMiniTitle>H.Point Pay 관리</MypageMiniTitle>
+      <MypageMiniTitle>나의 기념일</MypageMiniTitle>
+      <MypageMiniTitle>회원 탈퇴</MypageMiniTitle>
+      <MypageBorder />
+
+    </MypageSidebar>
+
+    <MypageContent>
+      <MypageAdv>
+      <MypageAdvImg src = {ClubAdvImg} />
+      <MypageAdvBtn onClick={() => navigate('/clubregister')}>등록하러 가기 ＞</MypageAdvBtn>
+      </MypageAdv>
+    </MypageContent>
+
+  </MypageGrid>
+  )
 }
 
 export default MyPageContainer;
---
+++ src/components/mypage/mypage.style.jsx
@@ -0,0 +1,89 @@
+import styled from 'styled-components';
+
+export const MypageGrid = styled.div`
+    display: grid;
+    /* grid-template-rows: 25% 25% auto; */
+    margin: 3% 3.5%;
+    /* height: 100%; */
+    @media (max-width: 1100px) {
+        margin: 0 2%;
+    }
+
+    grid-template-areas:
+    'sidebar content';
+    grid-gap: 0.5rem;
+    /* grid-gap: 1vw; */
+    height: auto;
+`;
+
+export const MypageSidebar = styled.div`
+    grid-area: sidebar;
+    width: 100%;
+`;
+
+export const MypageTitle = styled.div`
+    font-weight: 700;
+    font-size: 25px;
+    @media (max-width: 1100px) {
+        font-size: 20px;
+    }
+    /* border-right: 0.5px solid black; */
+    margin: 0 5px;
+`;
+
+export const MypageBoldBorder = styled.hr`
+    /* color: black; */
+    height: 2px;
+    background-color: black;
+    margin: 15px 0px 23px;
+`;
+
+export const MypageContent = styled.div`
+    max-width: 750px;
+    padding: 35px 20px;
+`;
+
+export const MypageSubtitle = styled.div`
+    font-weight: bolder;
+    font-size: 15px;
+    margin: 15px 10px;
+`
+
+export const MypageMiniTitle = styled.div`
+    font-weight: medium;
+    font-size: 12px;
+    color: gray;
+    margin: 10px 12px;
+    cursor: pointer;
+`
+
+export const MypageBorder = styled.hr`
+    color: gray;
+    height: 0.5px;
+    margin: 23px 10px;
+`;
+
+export const MypageAdv = styled.div`
+    position: relative;
+`;
+
+export const MypageAdvImg = styled.img`
+    object-fit: contain;
+    display: block;
+    padding: 10px;
+    width: 100%;
+`;
+
+export const MypageAdvBtn = styled.div`
+    position: absolute;
+    font-size: 15px;
+    bottom: 40px;
+    left: 40px;
+    @media (max-width: 1100px) {
+    font-size: 8px;
+    bottom: 24px;
+    left: 25px;
+    }
+    font-weight: bold;
+    cursor: pointer;
+`
