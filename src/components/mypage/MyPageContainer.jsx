4acbe2f9da12d6a2fe13f97e0869ba30bb539e7f
송민진 <m_6595@naver.com>
2023-09-10 21:38:23 +0900

    feat(mypage): 마이페이지 U구현

--- src/components/mypage/MyPageContainer.jsx
+++ src/components/mypage/MyPageContainer.jsx
@@ -1,10 +1,33 @@
-import React from 'react';
-import { MypageAdv, MypageAdvBtn, MypageAdvImg, MypageBoldBorder, MypageBorder, MypageContent, MypageGrid, MypageMiniTitle, MypageSidebar, MypageSubtitle, MypageTitle } from './mypage.style';
+import { React, useState } from 'react';
+import { MyInfoBox, MyInfoBoxCnt, MyInfoBoxTitle, MyInfoContainer, MyInfoElement, MyInfoTitle, MypageAdv, MypageAdvBtn, MypageAdvImg, MypageBoldBorder, MypageBorder, MypageContent, MypageGrid, MypageMiniTitle, MypageSidebar, MypageSubtitle, MypageTitle } from './mypage.style';
 import ClubAdvImg from '../../assets/club/join_club_adv_narrow.png'
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux/es/hooks/useSelector';
+import MyReservationContainer from './MyReservationContainer';
+import MyOrderContainer from './MyOrderContainer';
+import MyPetContainer from './MyPetContainer';
+import MySubscriptionContainer from './MySubscriptionContainer';
 
 function MyPageContainer() {
+  const member = useSelector((state) => state.member)
+  const pets = member.pet;
   const navigate = useNavigate();
+  const [selectedMenu, setSelectedMenu] = useState(null);
+
+  const renderContent = () => {
+    switch (selectedMenu) {
+      case 'mypet':
+        return <MyPetContainer />;
+      case 'myorder':
+        return <MyOrderContainer />;
+      case 'mysubscription':
+        return <MySubscriptionContainer />;
+      case 'myreservation':
+        return <MyReservationContainer />;
+      default:
+        return <MyPetContainer />;
+    }
+  }
 
   return (
   <MypageGrid>
@@ -14,18 +37,18 @@ function MyPageContainer() {
       <MypageBoldBorder />
 
       <MypageSubtitle>나의 반려동물 관리</MypageSubtitle>
-      <MypageMiniTitle>나의 반려동물 목록</MypageMiniTitle>
+      <MypageMiniTitle onClick={() => setSelectedMenu('mypet')}>나의 반려동물 목록</MypageMiniTitle>
       <MypageMiniTitle>나의 반려동물 맞춤 추천</MypageMiniTitle>
       <MypageBorder />
 
       <MypageSubtitle>나의 쇼핑내역</MypageSubtitle>
-      <MypageMiniTitle>주문/배송 조회</MypageMiniTitle>
-      <MypageMiniTitle>구독 관리</MypageMiniTitle>
+      <MypageMiniTitle onClick={() => setSelectedMenu('myorder')}>주문/배송 조회</MypageMiniTitle>
+      <MypageMiniTitle onClick={() => setSelectedMenu('mysubscription')}>구독 관리</MypageMiniTitle>
       <MypageBorder />
 
       <MypageSubtitle>나의 흰디카 예약</MypageSubtitle>
       <MypageMiniTitle>자주 찾는 지점</MypageMiniTitle>
-      <MypageMiniTitle>예약/취소 내역</MypageMiniTitle>
+      <MypageMiniTitle onClick={() => setSelectedMenu('myreservation')}>예약/취소 내역</MypageMiniTitle>
       <MypageBorder />
 
       <MypageSubtitle>회원 정보</MypageSubtitle>
@@ -38,11 +61,37 @@ function MyPageContainer() {
 
     </MypageSidebar>
 
+    <MyInfoContainer>
+    <MyInfoTitle>&nbsp;<b>{member.name}</b>님 반갑습니다.</MyInfoTitle>
+      <MyInfoBox>
+        <MyInfoElement>
+          <MyInfoBoxTitle>나의 반려동물</MyInfoBoxTitle>
+          <MyInfoBoxCnt>{pets.length}</MyInfoBoxCnt>
+        </MyInfoElement>
+        <MyInfoElement>
+          <MyInfoBoxTitle>배송중</MyInfoBoxTitle>
+          <MyInfoBoxCnt>2</MyInfoBoxCnt>
+        </MyInfoElement>
+        <MyInfoElement>
+          <MyInfoBoxTitle>구독</MyInfoBoxTitle>
+          <MyInfoBoxCnt>0</MyInfoBoxCnt>
+        </MyInfoElement>
+        <MyInfoElement>
+          <MyInfoBoxTitle>쿠폰</MyInfoBoxTitle>
+          <MyInfoBoxCnt>3</MyInfoBoxCnt>
+        </MyInfoElement>
+      </MyInfoBox>
+    </MyInfoContainer>
+      
     <MypageContent>
+    {renderContent()}
+
+      {/* 없을 때 광고 */}
       <MypageAdv>
       <MypageAdvImg src = {ClubAdvImg} />
       <MypageAdvBtn onClick={() => navigate('/clubregister')}>등록하러 가기 ＞</MypageAdvBtn>
       </MypageAdv>
+
     </MypageContent>
 
   </MypageGrid>
--- src/components/mypage/mypage.style.jsx
+++ src/components/mypage/mypage.style.jsx
@@ -10,8 +10,10 @@ export const MypageGrid = styled.div`
     }
 
     grid-template-areas:
+    'sidebar info'
+    'sidebar content'
     'sidebar content';
-    grid-gap: 0.5rem;
+    grid-gap: 50px;
     /* grid-gap: 1vw; */
     height: auto;
 `;
@@ -41,7 +43,6 @@ export const MypageBoldBorder = styled.hr`
 
 export const MypageContent = styled.div`
     max-width: 100%;
-    padding: 35px 20px;
 `;
 
 export const MypageSubtitle = styled.div`
@@ -71,7 +72,6 @@ export const MypageAdv = styled.div`
 export const MypageAdvImg = styled.img`
     object-fit: contain;
     display: block;
-    padding: 10px;
     width: 100%;
 `;
 
@@ -87,4 +87,89 @@ export const MypageAdvBtn = styled.div`
     }
     font-weight: bold;
     cursor: pointer;
+`;
+
+export const MyInfoContainer = styled.div`
+    margin-top: 21px;
+    margin-bottom: -20px;
+`;
+
+export const MyInfoTitle = styled.div`
+    font-size: 13px;
+    grid-area: info;
+`;
+
+export const MyInfoBox = styled.div`
+    border-radius: 20px;
+    background-color: #ececec;
+    display: flex;
+    width: 100%;
+    min-height: 100px;
+    margin-top: 10px;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const MyInfoElement = styled.div`
+    text-align: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    width: 100%;
+`;
+
+export const MyInfoBoxTitle = styled.div`
+    font-size: 11px;
+    font-weight: medium;
+    color: gray;
+    margin-bottom: 8px;
+`;
+
+export const MyInfoBoxCnt = styled.div`
+    font-size: 25px;
+    font-weight: bolder;
+`;
+
+export const MypageCard = styled.div`
+    display: flex;
+    justify-content: start;
+    border-top: 1px solid gray;
+`;
+
+export const MypageCardImg = styled.img`
+    border-radius: 10px;
+    width: 100px;
+    height: 100px;
+    border-top: 1px solid gray;
+`;
+
+export const MypageCardElement = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: start;
+    padding: 10px;
 `
+
+export const MypageCardTitle = styled.div`
+    font-size: 13px;
+    font-weight: bold;
+`
+
+export const MypageCardDescr = styled.div`
+    font-size: 11px;
+    font-weight: medium;
+`
+
+export const MypageList = styled.div`
+    margin-bottom: 40px; 
+    border-bottom: 1px solid gray;
+`;
+
+export const MypageListIndex = styled.div`
+    background-color: #bfc9be;
+    font-size: 15px;
+    padding: 3px;
+`;
