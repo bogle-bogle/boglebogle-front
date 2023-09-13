1791098c1158ec50a9f8dabe2303de55d0023ca6
BoSeon Park <bedurgi999@gmail.com>
2023-09-13 15:21:59 +0900

    yml16

--- src/components/login/RedirectUrl.jsx
+++ src/components/login/RedirectUrl.jsx
@@ -18,7 +18,7 @@ function RedirectUrl() {
     const grantType = "authorization_code";
     const REST_API_KEY = `${process.env.REACT_APP_KAKAO_REST_API_KEY}`;
     const REDIRECT_URI = `${process.env.REACT_APP_KAKAO_REDIRECT_URI}`;
-    console.log(document.location.toString());
+
     console.log(new URL(document.location.toString()));
     console.log(new URL(document.location.toString()).searchParams);
     console.log(params);
