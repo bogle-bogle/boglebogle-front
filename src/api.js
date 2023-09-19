93bb93a0376b193eb27a184fe72c26d686fc8631
송민진 <m_6595@naver.com>
2023-09-19 11:58:38 +0900

    feat: 토큰 만료시 에러 메시지 뜨도록 변경
    #109

--- src/api.js
+++ src/api.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
 const serverUrl = String(process.env.REACT_APP_SERVER_URL);
 
@@ -11,38 +12,65 @@ async function get(endpoint, params = "") {
     requestURL = serverUrl + endpoint + "/" + params;
   }
 
-  return axios.get(requestURL, {
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem("userToken")}`,
-    },
-  });
+  return axios
+    .get(requestURL, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    })
+    .catch((error) => {
+      if (error.response?.data?.code == "EXPIRED_TOKEN") {
+        toast.error("로그인이 만료되었습니다. 재로그인해주세요.");
+        console.error(error.response);
+      }
+    });
 }
 
 async function post(endpoint, data) {
-  // const bodyData = JSON.stringify(data);
   console.log(serverUrl);
 
-  return axios.post(serverUrl + endpoint, data, {
-    headers: {
-      // "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("userToken")}`,
-    },
-  });
+  return axios
+    .post(serverUrl + endpoint, data, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    })
+    .catch((error) => {
+      if (error.response?.data?.code == "EXPIRED_TOKEN") {
+        toast.error("로그인이 만료되었습니다. 재로그인해주세요.");
+        console.error(error.response);
+      }
+    });
 }
 
 async function put(endpoint, data) {
-  // const bodyData = JSON.stringify(data);
-  console.log(serverUrl);
-
-  return axios.put(serverUrl + endpoint, data, {
-    headers: {
-      // "Content-Type": "application/json",
-      Authorization: `Bearer ${localStorage.getItem("userToken")}`,
-    },
-  });
+  return axios
+    .put(serverUrl + endpoint, data, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    })
+    .catch((error) => {
+      if (error.response?.data?.code == "EXPIRED_TOKEN") {
+        toast.error("로그인이 만료되었습니다. 재로그인해주세요.");
+        console.error(error.response);
+      }
+    });
 }
 
 async function del(endpoint, params = "") {
-  return axios.delete(serverUrl + endpoint + "/" + params);
+  return axios
+    .delete(serverUrl + endpoint + "/" + params, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("userToken")}`,
+      },
+    })
+    .catch((error) => {
+      if (error.response?.data?.code == "EXPIRED_TOKEN") {
+        toast.error("로그인이 만료되었습니다. 재로그인해주세요.");
+        console.error(error.response);
+      }
+    });
 }
+
 export { get, post, put, del as delete };
