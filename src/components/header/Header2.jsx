e9bfa8cd0f57f4a3c7c3b31b4acc01d9e4a677b5
송민진 <m_6595@naver.com>
2023-09-26 22:43:10 +0900

    style: prettier 설정
    #139

--- src/components/header/Header2.jsx
+++ src/components/header/Header2.jsx
@@ -1,50 +1,50 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
-import { useState } from "react";
-import { HiMenu, HiShoppingCart } from "react-icons/hi";
-import { FiLogOut } from "react-icons/fi";
-import { IoClose } from "react-icons/io5";
-import logo from "../../assets/thepet_logo_img.png";
-import { BiSolidUser } from "react-icons/bi";
-import { Header, StyledNavLink } from "./Header2.style";
-import { useDispatch, useSelector } from "react-redux";
-import { memberAction } from "../../feature/member/member";
-import { loginAction } from "../../feature/member/login";
-import LoginModal from "../login/LoginModal";
-import Modal from "../modal/Modal";
-import { useEffect } from "react";
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { HiMenu, HiShoppingCart } from 'react-icons/hi';
+import { FiLogOut } from 'react-icons/fi';
+import { IoClose } from 'react-icons/io5';
+import logo from '../../assets/thepet_logo_img.png';
+import { BiSolidUser } from 'react-icons/bi';
+import { Header, StyledNavLink } from './Header2.style';
+import { useDispatch, useSelector } from 'react-redux';
+import { memberAction } from '../../feature/member/member';
+import { loginAction } from '../../feature/member/login';
+import LoginModal from '../login/LoginModal';
+import Modal from '../modal/Modal';
+import { useEffect } from 'react';
 
 function Header2() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const member = useSelector((state) => state.member); // 추가
-  const login = useSelector((state) => state.login);
+  const member = useSelector(state => state.member);
+  const login = useSelector(state => state.login);
   const menuList = [
     {
-      title: '월간 구독',
-      link: '/thepetbox',
+      title: '쇼핑',
+      link: '/shop',
     },
     {
       title: 'AI 맞춤 추천',
       link: '/suggestion',
     },
     {
-      title: "쇼핑",
-      link: "/shop",
+      title: '월간 더펫박스',
+      link: '/thepetbox',
     },
     {
-      title: "흰디카 예약",
-      link: "/heendycar",
+      title: '흰디카 예약',
+      link: '/heendycar',
     },
     {
-      title: "흰디 놀이터",
-      link: "/playground",
+      title: '흰디 놀이터',
+      link: '/playground',
     },
   ];
 
   useEffect(() => {
-    if (localStorage.getItem("userToken") === null) {
+    if (localStorage.getItem('userToken') === null) {
       dispatch(memberAction.clearMember());
     }
   }, []);
@@ -85,7 +85,7 @@ function Header2() {
             src={logo}
             alt="Logo"
             className="logo"
-            onClick={() => navigate("/")}
+            onClick={() => navigate('/')}
           />
         </div>
 
@@ -111,7 +111,7 @@ function Header2() {
 
         {/* User 메뉴 리스트 */}
         <div className="header__right">
-          {member.name !== "" ? (
+          {member.name !== '' ? (
             <div className="list__container">
               <StyledNavLink to="/cart" className="menu_icon cart_icon">
                 <HiShoppingCart></HiShoppingCart>
@@ -122,7 +122,7 @@ function Header2() {
             <></>
           )}
           <div className="list__container">
-            {member.name !== "" ? (
+            {member.name !== '' ? (
               <>
                 <StyledNavLink to="/mypage">{member.name}님</StyledNavLink>
                 <StyledNavLink
@@ -130,7 +130,7 @@ function Header2() {
                   className="menu_icon logout_icon"
                   onClick={() => {
                     dispatch(memberAction.clearMember());
-                    localStorage.removeItem("userToken");
+                    localStorage.removeItem('userToken');
                     // navigate('/')
                   }}
                 >
