b07e7e489a1cc3ddc559f240adb789a28c574e57
송민진 <m_6595@naver.com>
2023-09-29 01:58:20 +0900

    fix(page): 사용하지 않는 페이지 삭제

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import CustomReadyPage from "./pages/CustomReadyPage";
-import CustomReadyResult from "./pages/CustomReadyResult";
 import Main from "./pages/Main";
 import RedirectUrl from "./components/login/RedirectUrl";
 import Shop from "./pages/Shop";
 import Cart from "./pages/CartPage";
 import Ordersheet from "./pages/OrderSheetPage";
 import OrderComplete from "./pages/OrderComplete";
-import Subscription from "./pages/Subscription";
 import ClubRegister from "./components/club/ClubRegister";
 import CompleteClubRegister from "./components/club/CompleteClubRegister";
 import MyPet from "./pages/MyPet";
@@ -36,16 +33,12 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/suggestion" element={<Suggestion />} />
-          <Route path="/customready" element={<CustomReadyPage />} />
-          <Route path="/customresult" element={<CustomReadyResult />} />
-          <Route path="/" element={<Main />}></Route>
           <Route path="/auth" element={<RedirectUrl />}></Route>
           <Route path="/shop" element={<Shop />}></Route>
           <Route path="/product/:id" element={<ProductDetail />}></Route>
           <Route path="/cart" element={<Cart />}></Route>
           <Route path="/ordersheet" element={<Ordersheet />}></Route>
           <Route path="/ordercomplete" element={<OrderComplete />}></Route>
-          <Route path="/sub" element={<Subscription />} />
           <Route path="/thepetbox" element={<ThePetBox />} />
           <Route path="/heendycar" element={<HeendyCarPage />}></Route>
           <Route path="/card" element={<HyundaiCard />}></Route>
@@ -53,10 +46,7 @@ function App() {
           <Route path="/addpet" element={<AddPet />}></Route>
           <Route path="/playground" element={<PlayGround />}></Route>
           <Route path="/mypage" element={<MyPage />}></Route>
-          <Route
-            path="/completeclubregister"
-            element={<CompleteClubRegister />}
-          ></Route>
+          <Route path="/completeclubregister" element={<CompleteClubRegister />}></Route>
           <Route path="/mypet/:memberId" element={<MyPet />}></Route>
           <Route path="/mbti" element={<MbtiTest />}></Route>
           <Route path="/ex" element={<Example />}></Route>
