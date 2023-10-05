daa4cbbd7c6d49ee04d8b20790c632417f6b64b4
송민진 <m_6595@naver.com>
2023-10-05 10:58:12 +0900

    feat: swal창 반응형 적용
    #169

--- src/components/global/WalkingHeendySwal.jsx
+++ src/components/global/WalkingHeendySwal.jsx
@@ -1,35 +1,56 @@
-import React, { useEffect } from 'react';
-import swal from 'sweetalert2';
-import walkingheendy from "../../assets/custom/walkingheendy.gif";
+const WalkingHeendySwal = ({
+  title,
+  confirmButtonText,
+  cancelButtonText,
+  onConfirm,
+  onCancel,
+  trigger,
+}) => {
+  const isMobile = window.innerWidth <= 768;
 
-const WalkingHeendySwal = ({ title, confirmButtonText, cancelButtonText, onConfirm, onCancel, trigger }) => {
-  
+  // 모바일 환경에서만 적용할 스타일
+  const mobileStyle = isMobile ? { width: '60%' } : {};
+
+  const swalOptions = {
+    title: title,
+    showCancelButton: true,
+    imageUrl: walkingheendy,
+    confirmButtonText: confirmButtonText,
+    cancelButtonText: cancelButtonText,
+    confirmButtonColor: '#499878',
+    cancelButtonColor: '#A4A4A4',
+    customClass: {
+      confirmButton: 'swal2-button',
+      cancelButton: 'swal2-button',
+      popup: 'swal-mobile', // 클래스명 추가
+    },
+    style: mobileStyle, // 모바일 스타일 적용
+  };
+
   useEffect(() => {
     if (trigger) {
       swal
-        .fire({
-          title: title,
-          showCancelButton: true,
-          imageUrl: walkingheendy,
-          confirmButtonText: confirmButtonText,
-          cancelButtonText: cancelButtonText,
-          confirmButtonColor: "#499878",
-          cancelButtonColor: "#A4A4A4",
-          customClass: {
-            confirmButton: "swal2-button",
-            cancelButton: "swal2-button",
-          },
+        .fire(swalOptions)
+        .then(result => {
+          if (result.isConfirmed && onConfirm) {
+            onConfirm();
+          } else if (result.isDismissed && onCancel) {
+            onCancel();
+          }
         })
-        .then((result) => {
-            if (result.isConfirmed && onConfirm) {
-              onConfirm();
-            } else if (result.isDismissed && onCancel) {
-              onCancel();
-            }
-          })
-;
+        .catch(error => {
+          // 필요한 경우 에러를 처리합니다
+          console.error('에러:', error);
+        });
     }
-  }, [trigger, title, confirmButtonText, cancelButtonText, onConfirm, onCancel]);
+  }, [
+    trigger,
+    title,
+    confirmButtonText,
+    cancelButtonText,
+    onConfirm,
+    onCancel,
+  ]);
 
   return null;
 };
--- src/components/global/showClappingHeendySwal.jsx
+++ src/components/global/showClappingHeendySwal.jsx
@@ -1,18 +1,27 @@
 import swal from 'sweetalert2';
 import clappingHeendy from '../../assets/custom/clappingheendy.gif';
 
-export const showClappingHeendySwal = (title) => {
-  swal.fire({
+export const showClappingHeendySwal = title => {
+  const isMobile = window.innerWidth <= 768;
+
+  const swalOptions = {
     title: title,
     showCancelButton: true,
     imageUrl: clappingHeendy,
-    confirmButtonText: "확인",
-    cancelButtonText: "취소",
-    confirmButtonColor: "#499878",
-    cancelButtonColor: "#A4A4A4",
+    confirmButtonText: '확인',
+    cancelButtonText: '취소',
+    confirmButtonColor: '#499878',
+    cancelButtonColor: '#A4A4A4',
     customClass: {
-      confirmButton: "swal2-button",
-      cancelButton: "swal2-button",
+      confirmButton: 'swal2-button',
+      cancelButton: 'swal2-button',
+      popup: isMobile ? 'swal-mobile' : '',
     },
-  });
+  };
+
+  if (isMobile) {
+    swalOptions.width = '55%';
+  }
+
+  swal.fire(swalOptions);
 };
