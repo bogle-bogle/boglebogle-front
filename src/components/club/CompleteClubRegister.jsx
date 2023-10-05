01638355fc9092ce3bb83788bada38ae7214fb0a
송민진 <m_6595@naver.com>
2023-10-05 10:58:30 +0900

    펫 등록 완료 페이지 구현
    #169

--- src/components/club/CompleteClubRegister.jsx
+++ src/components/club/CompleteClubRegister.jsx
@@ -1,14 +1,43 @@
 import React from 'react';
-import infoImg from '../../assets/club/가입완료.png';
+import shopperheendy from '../../assets/custom/shopperheendy.png';
 import { useNavigate } from 'react-router-dom';
 import { LogoContainer, LogoImg } from './index.style';
+import {
+  HeendyArea,
+  ShopperHeendy,
+  TitleBox,
+} from '../suggestion/suggestion.style';
+import { GreenBtn, GreyBtn } from '../global/btn.style';
 
 function CompleteClubRegister() {
   const navigate = useNavigate();
   return (
-    <LogoContainer onClick={() => navigate('/')}>
-      <LogoImg src={infoImg} alt="Logo" className="logo" />
-    </LogoContainer>
+    <HeendyArea>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <ShopperHeendy src={shopperheendy} alt=" " />
+      <TitleBox>
+        <p className="title1">등록이 완료되었습니다!</p>
+        <p className="title2">
+          <strong>
+            입력하신 정보를 기반으로 <mark>나의 반려동물 맞춤 상품</mark>을
+            추천해드릴게요!
+          </strong>
+        </p>
+      </TitleBox>
+      <br />
+      <GreenBtn onClick={() => navigate('/shop')}>쇼핑하러 가기</GreenBtn>
+    </HeendyArea>
   );
 }
 
