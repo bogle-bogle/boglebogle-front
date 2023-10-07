2d3892495e75733bf6984081568ccb0f55a30724
송민진 <m_6595@naver.com>
2023-10-07 18:51:45 +0900

    feat: 플로팅 추가
    #258

--- src/pages/MainLayout.jsx
+++ src/pages/MainLayout.jsx
@@ -4,8 +4,10 @@ import Header from '../components/header/Header2';
 import { Helmet } from 'react-helmet-async';
 import GlobalStyle from '../styles/GlobalStyle';
 import Footer from '../components/footer/footer';
-import floatingSub from '../assets/floating/floating_sub.png';
-import floatingPg from '../assets/floating/floating_playground.png';
+import floatingAdvAi from '../assets/floating/ai-suggestion.png';
+import floatingTpb from '../assets/floating/thepetbox.png';
+// import floatingSub from '../assets/floating/ai-suggestion.png';
+// import floatingPg from '../assets/floating/floating_playground.png';
 import RouteChangeTracker from '../RouteChangeTracker';
 
 import goodHeendy from '../assets/main/goodheendy.png';
@@ -31,15 +33,23 @@ function MainLayout() {
             className="floatingImgCard"
             onClick={() => navigate('/suggestion')}
           >
-            <img className="floatingImg" src={goodHeendy} alt="" />
-            <p className="floatingText">AI 추천</p>
+            <img className="floatingImg" src={floatingAdvAi} alt="" />
           </div>
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
+          <br />
           <div
             className="floatingImgCard"
             onClick={() => navigate('/thepetbox')}
           >
-            <img className="floatingImg" src={goodHeendy} alt="" />
-            <p className="floatingText">더펫 박스</p>
+            <img className="floatingImg" src={floatingTpb} alt="" />
           </div>
         </div>
       </div>
